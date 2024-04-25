4a2975759f876fe7b24c8b9898220a4c2e659cf7
Haaaaaai <155551391+Haaaaaai@users.noreply.github.com>
2024-04-25 10:40:24 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-//$ : 제이쿼리
-//document : 문서(<body> ~ </body> 를 가리킴)
-//ready() : 문서가 준비되면 ... 이라는 뜻의 이벤트
-//function() {} : 함수, ready이벤트가 발생하면 실행되는 문장들
-
 $(document).ready(function(){
-    //header 영역의 모바일 버전 글로벌 내비게이션
-    //모바일 메뉴 버튼을 클릭하면 메뉴 영역이 오른쪽에서 나타남
+    $(".search_btn").click(function(){
+        $(".search_area").animate({"width":180});
+        $(".login input").animate({"width":140});
+        $(".login ul").animate({"margin-right":200});
+    });
+
+    //pc버전 글로벌 내비게이션 주메뉴 - 마우스 오버, 서브메뉴 슬라이드다운
+    $(".mainmenu nav > ul > li").mouseenter(function(){
+        $(this).find(".sub").stop().slideDown(300);
+    });
+    //pc버전 글로벌 내비게이션 주메뉴 - 마우스 아웃, 서브메뉴 슬라이드업
+    $(".mainmenu nav > ul > li").mouseleave(function(){
+        $(this).find(".sub").stop().slideUp();
+    });
+
     $('.mobile_menu_btn').click(function(){
         $('.mobile_menu').animate({
             "right":0
@@ -13,7 +21,7 @@ $(document).ready(function(){
         $('.close_btn i').addClass("active");
     
     });
-    //모바일 메뉴의 x(닫기)버튼을 클릭하면 메뉴 영역이 오른쪽으로 들어감
+    //모바일 닫기 버튼 - 메뉴 영역 오른쪽으로 이동
     $('.close_btn i').click(function() {
         $(".mobile_menu").animate({
             "right":"-100vw"
@@ -27,69 +35,76 @@ $(document).ready(function(){
         $(this).next().slideToggle();
         $(this).toggleClass("active");
         return false;
-    })
-    
-
-    //$(".sitemap_list > ul > li > a") : 선택자
-    //click() : 클릭이벤트
-    $(".sitemap_list > ul > li > a").click(function(){
-        //만약 클릭한 a태그에 active클래스가 추가되어 있지 않다면
-        if($(this).attr("class") != "active"){            
-            //not(this) : 클릭한 메뉴 이외의 객체들
-            //slideUp() : 객체를 접음.
-            $(".sitemap_list > ul > li > a").next().slideUp();
-            //$(this) : 클릭이벤트가 발생한 객체를 가리킴
-            //next() : 클릭이벤트가 발생한 객체의 다음 형제객체
-            $(this).next().slideDown();
-            //removeClass("active") : active 클래스를 제거함
-            $(".sitemap_list > ul > li > a").removeClass("active");
-            //addClass("active") : active 클래스를 추가함
-            $(this).addClass("active");                    
-            //a태그를 클릭할 때 맨 위로 이동하는 것을 방지함.
-            return false;
-
-        //클릭한 a태그에 active가 이미 추가되어 있는 상태라면    
-        }else{
-            //다시 클릭한 주메뉴의 서브메뉴 들어감
-            $(this).next().slideUp();
-            //다시 클릭한 주메뉴에서 active클래스 제거
-            $(this).removeClass("active");
-            //a태그를 클릭할 때 맨 위로 이동하는 것을 방지함.
-            return false;
-        }        
     });
-    //swiper slide
+    
+    //html문서 가로길이 660px 이하 아코디언 메뉴 적용
+    if($("html, body").width() <= 660){
+        $(".sitemap_list > ul > li > a").click(function(){
+            if($(this).attr("class") != "active"){        
+                $(".sitemap_list > ul > li > a").next().slideUp();
+                $(this).next().slideDown();
+                $(".sitemap_list > ul > li > a").removeClass("active");
+                $(this).addClass("active");                    
+                return false;
+            //클릭한 a태그에 active가 이미 추가되어 있는 상태라면    
+            }else{
+                $(this).next().slideUp();
+                $(this).removeClass("active");
+                return false;
+            }        
+        });
+    }   
+
+//swiper slide
     const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        // 마지막번호에서 다시 1번으로 오세요
         loop: true,
-        //자동 슬라이드
         autoplay: {
-            delay:2000 //3초(millisecond 단위 :1/1000초)
+            delay:2000
         },
-        //페이지네이션 동글동글한 작은 버튼
-        // pagination: {
-        //   el: '.swiper-pagination',
-        // },
+        breakpoints:{
+            1024:{
+                slidesPerView:3,
+                slidesPerGroup:3
+            },
+            768:{
+                slidesPerView:3,
+                slidesPerGroup:3
+            },
+            480:{   
+                slidesPerView:1,
+                slidesPerGroup:1
+            },
+            360:{
+                slidesPerView:1,
+                slidesPerGroup:1
+            }
+        }
         
     });
-      //swiper 슬라이드 버튼
+
+    //swiper 슬라이드 멈춤 버튼 클릭
     $('.fa-pause').click(function() {
-    //클릭한 객체(.fa-pause)를 숨김
     $(this).hide();
-    //.fa-play객체 보임(인라인요소+블록요소)
-    //css에 접근, display 설정을 inline-block으로 변경
-    //(block-기본설정) show와 동일
     $('.fa-play').css('display','inline-block');
-    //swiper자동 슬라이드 멈춤
     swiper.autoplay.stop();
     })
+
+    //슬라이드 재생 버튼 클릭, 다시 재생
     $('.fa-play').click(function() {
-        //클릭한 객체(.fa-play)를 숨김
         $(this).hide();
-        //.fa-pause객체 보임(인라인+블록요소)
         $('.fa-pause').css('display','inline-block');
-        //swiper자동 슬라이드 시작
         swiper.autoplay.start();
-    })
-})
+    });
+    
+    //스크롤 이벤트, 스크롤에 따라 애니메이션 작동
+    $(window).scroll(function(){
+        let winTop=$(this).scrollTop();
+        $('section').each(function(){
+            let secTop=$(this).offset().top-300;
+            if(secTop < winTop) {
+                $(this).find('.ani').addClass('active');
+                $(this).find('.ani').addClass('delay');
+            }   //if
+        }); //each
+    }); //scroll
+});
